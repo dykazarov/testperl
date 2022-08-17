f2677044cb7194b560c2e59d4846ba473c3cce05
Дмитрий Казаров <dmitriy.kazarov@shbp.ru>
2022-08-17 15:54:19 +0300

    Сделал парсер лога

---
+++ 1.sql
@@ -0,0 +1,20 @@
+CREATE TABLE message (
+    created TIMESTAMP NOT NULL,
+    id VARCHAR(255) NOT NULL,
+    int_id CHAR(16) NOT NULL,
+    str VARCHAR(255) NOT NULL,
+    status BOOL,
+    CONSTRAINT message_id_pk PRIMARY KEY(id)
+);
+CREATE INDEX message_created_idx ON message (created);
+CREATE INDEX message_int_id_idx ON message (int_id);
+
+
+CREATE TABLE log (
+    created TIMESTAMP NOT NULL,
+    int_id CHAR(16) NOT NULL,
+    str VARCHAR(255),
+    address VARCHAR(255)
+);
+CREATE INDEX log_address_idx ON log (address);
+
